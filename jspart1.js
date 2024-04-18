f477f0c2fc08f138ef26ac159681e761f3202540
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-04-18 22:31:15 +0530

    Add files via upload

---
+++ jspart1.js
@@ -0,0 +1,42 @@
+//QS. What is the value of age after this code runs?
+let age = 23;
+age + 2;  //after 2 year
+console.log(age);
+
+
+//Qs. What is the value of avg after this code runs?
+let hindi = 80;
+let eng = 90;
+let math = 100;
+let avg = (hindi + eng + math)/3;
+console.log(avg);
+
+//Qs. What is the value of each varibles in each line of code?
+let num = 5;
+let newNum = num++;
+newNum = ++num;
+console.log(newNum);
+
+//Qs. Declare your name as a string and print its length in javascript? 
+let name = "Vivek Dhamande";
+console.log(name.length);
+
+//Qs. Declare your first name as a string and print its first character?
+let firstName = "Vivek";
+console.log(firstName[0]);
+
+//Qs. Declare your first name as string and print its last character?
+let fName = "Vivek";
+let lastCahracter = (fName.length)-1;
+console.log(fName[lastCahracter]);
+
+//Qs. What is the output of following code?
+console.log("VivekDhamande"+123);
+
+
+//Qs. What are length of an empty string & a string with a single space?
+let str1 = "";
+console.log(str1.length);
+
+let str2 = " ";
+console.log(str2.length);
